bdd6c602b7736b4d38fca18d00ca802d5e1809aa
Tille1408 <mike.tilgner@gmail.com>
2020-10-20 10:37:13 +0200

    Master wiederhertgestellt

--- predict.py
+++ predict.py
@@ -6,11 +6,6 @@ tagger: SequenceTagger = SequenceTagger.load("ner")
 sentence: Sentence = Sentence("George Washington went to Washington .")
 tagger.predict(sentence)
 
-<<<<<<< HEAD
-print("Analysing the hotfix sentence %s“ % sentence)
-=======
-print("Irgendwas")
 print("Analysing sentence %s“ % sentence)
->>>>>>> hotfix
 print("\nThe following NER tags are found: \n")
 print(sentence.to_tagged_string())
